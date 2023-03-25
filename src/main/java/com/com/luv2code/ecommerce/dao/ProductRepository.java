f793efbd08371a9e5c23df5c77ae800d13a26f65
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-25 15:51:09 -0400

    add java backend code first commit

---
+++ src/main/java/com/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.com.luv2code.ecommerce.dao;
+
+import com.com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+// Give the actual url where our application is running.
+// Accept calls from web browser scripts for this origin.
+// origin is protocol + hostname + port ..
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
