6bcdb312bab1eefa0fcb8446f0be73603346a125
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-25 17:23:25 -0400

    add changes to product repository class

--- src/main/java/com/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 // Give the actual url where our application is running.
 // Accept calls from web browser scripts for this origin.
 // origin is protocol + hostname + port ..
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")    // here we give the origin .. or server that our angular application is running on
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
