05b8779ff9fb08e36e6c681bdddb527215a35382
Jeff Campbell <86787426+citizenjett@users.noreply.github.com>
2023-03-27 14:12:52 -0400

    add last working point in Spring code Mar27

--- src/main/java/com/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,13 +1,16 @@
 package com.com.luv2code.ecommerce.dao;
 
 import com.com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 // Give the actual url where our application is running.
 // Accept calls from web browser scripts for this origin.
 // origin is protocol + hostname + port ..
-@CrossOrigin("http://localhost:4200")    // here we give the origin .. or server that our angular application is running on
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
